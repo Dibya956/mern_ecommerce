4837ba410ce3150be74032f35c95b81e4960ace7
Dibya956 <dibyamarkona@gmail.com>
2026-03-11 20:32:54 +0530

    feat: migrate Redis to Upstash and fix configuration errors

---
+++ backend/lib/redis.js
@@ -0,0 +1,16 @@
+// import Redis from "ioredis";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+
+
+import { Redis } from '@upstash/redis'
+import dotenv from "dotenv";
+dotenv.config()
+
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_URL,
+  token: process.env.UPSTASH_REDIS_TOKEN,
+})
